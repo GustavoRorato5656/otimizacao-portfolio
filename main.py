3401298b93911d09a1411def10556fc573fcf02e
Guilhermegoya22 <98859623+Guilhermegoya22@users.noreply.github.com>
2024-12-05 19:52:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 from pypfopt import EfficientFrontier, risk_models, expected_returns
+st.write("""# OLa""")
 st.title("Otimização de Portfólio com Ativos Selecionados")
 st.text("By: Guilherme Goya e Gustavo Rorato")
 # Entrada do usuário para os tickers dos ativos
