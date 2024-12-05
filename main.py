24f3de63320c518c9d7bc873c9cb4227d0c4bb69
Guilhermegoya22 <98859623+Guilhermegoya22@users.noreply.github.com>
2024-12-05 17:34:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Otimização de Portfólio")
 st.write("Bem-vindo ao aplicativo de otimização de portfólio usando Streamlit!")
-
+st.write("Rorato chupa rola")
 # Adicione sua lógica aqui
